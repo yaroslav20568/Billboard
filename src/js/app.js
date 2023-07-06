fd221e335d8fa90b4bf8501b861363a6cd745396
Yaroslav <ravlushevich.iarek@mail.ru>
2023-07-06 16:10:24 +0300

    header, responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,16 @@
 import { animations } from './functions/animations.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-	
+	const burgerBtn = document.querySelector('.header__burger');
+	const headerNav = document.querySelector('.header__nav');
+
+	burgerBtn.addEventListener('click', () => {
+		if(!burgerBtn.classList.contains('active')) {
+			burgerBtn.classList.add('active');
+			headerNav.classList.add('active');
+		} else {
+			burgerBtn.classList.remove('active');
+			headerNav.classList.remove('active');
+		}
+	});
 });
